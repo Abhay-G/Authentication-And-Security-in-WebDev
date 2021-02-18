00b0f43f767867ce40fd155f403c7685292d7418
Abhay Gupta <abhaygupta15153986@gmail.com>
2021-02-18 16:17:55 +0530

    add aditional OAuth of Google, sigin using google

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption"); FOR ENCRYPTING PASSWORD BUT WE ARE NOT USING IT
-// const md5 = require("md5");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,23 +33,56 @@ mongoose.connect(process.env.MONGO, {
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
+    googleId:String,
+    secrets:[String]
 });
 
-// userSchema.plugin(encrypt, {
-//     secret: process.env.SECRET,
-//     encryptedFields: ["password"],
-// });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            // console.log(profile);
+            User.findOrCreate({ googleId: profile.id }, function (err, user) {
+                return cb(err, user);
+            });
+        }
+    )
+);
 
 app.get("/", function (req, res) {
     res.render("home");
 });
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["email", "profile"] })
+);
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets page.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function (req, res) {
     res.render("login");
@@ -62,13 +93,41 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
+   User.find({"secrets":{$ne:null}},function(err,foundUsers){
+       if(err){
+           console.log(err);
+       }
+       else{
+           if(foundUsers){
+              res.render("secrets",{usersWithSecrets:foundUsers});
+           }
+       }
+   });
+});
+
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else{
         res.redirect("/login");
     }
 });
-
+app.post("/submit", function (req,res){
+    const secretSubmitted = req.body.secret;
+    // console.log(req.user.id); //this will give the mongodb id of that user. You can access information of the current user with req.user
+     User.findById(req.user.id,function(err,foundUser){
+         if(err){
+             console.log(err);
+         }else{
+             if(foundUser){
+                 foundUser.secrets.push(secretSubmitted);
+                 foundUser.save(function(){
+                     res.redirect("/secrets");
+                 });
+             }
+         }
+     })
+})
 app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
